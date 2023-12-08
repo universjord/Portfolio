5ddd1f000018300d5a135e7b73e32cc626f20c65
ratsiz_j <ratsiz_j@etna-alternance.net>
2023-12-08 12:20:53 +0100

    initial commit

---
+++ MonPortfolio/README.md
@@ -0,0 +1,36 @@
+## Porfolio Jordan RATSIZAFY  🙏
+
+# Mon portfolio
+
+Bienvenue sur mon portfolio ! Ce site a été créé pour mettre en avant mes compétences, projets, expériences professionnelles et les services que j'offre. Vous pouvez le visualiser directement sur ce lien ( ).
+
+## Technologies Utilisées
+
+- HTML
+- CSS
+- JavaScript
+
+## Structure du Projet
+
+Le projet est organisé de la manière suivante :
+
+- **index.html** : Page d'accueil du portfolio.
+- **css/sword** : dossier contenant le fichier CSS.
+- **js/global** : dossier contenant le fichier JS.
+- **img/** : Répertoire pour stocker les images utilisées dans le portfolio.
+## Fonctionnalités
+
+
+Fait en une seull page dont vous pouvez voir directement en appuyant sur le menu : 
+- Une page d'accueil
+- Présentation de ma biographie  
+- Mes services
+- Mes compétences
+- Mon portfolio.
+- Mon contact
+
+## Comment Exécuter Localement
+
+1. Cloner le dépôt : (à faire plus tard)
+2. Ouvrir le fichier index.html dans votre navigateur.
+
---
+++ MonPortfolio/js/global.js
@@ -0,0 +1,138 @@
+
+/* Menu mobile */
+
+function menuMobile() {
+  const btn = document.querySelector('.burger');
+  const header = document.querySelector('.header');
+  const links = document.querySelectorAll('.navbar a');
+
+  btn.addEventListener('click', () => {
+    header.classList.toggle('show-nav');
+  });
+
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      header.classList.remove('show-nav');
+    });
+  });
+}
+
+menuMobile();
+
+/* Porfolio */
+
+function tabsFilters() {
+  const tabs = document.querySelectorAll('.portfolio-filters a');
+  const projets = document.querySelectorAll('.portfolio .card');
+
+  const resetActiveLinks = () => {
+    tabs.forEach(elem => {
+      elem.classList.remove('active');
+    })
+  }
+
+  const showProjets = (elem) => {
+    console.log(elem);
+    projets.forEach(projet => {
+
+      let filter = projet.getAttribute('data-category');
+
+      if (elem === 'all') {
+        projet.parentNode.classList.remove('hide');
+        return
+      }
+
+      // option pour les plus motivés - opérateur ternaire
+      filter !== elem ? projet.parentNode.classList.add('hide') : projet.parentNode.classList.remove('hide');
+
+    });
+  }
+
+  tabs.forEach(elem => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+      let filter = elem.getAttribute('data-filter');
+      showProjets(filter)
+      resetActiveLinks();
+      elem.classList.add('active');
+    });
+  })
+}
+
+tabsFilters()
+
+function showProjectDetails() {
+  const links = document.querySelectorAll('.card__link');
+  const modals = document.querySelectorAll('.modal');
+  const btns = document.querySelectorAll('.modal__close');
+
+  const hideModals = () => {
+    modals.forEach(modal => {
+      modal.classList.remove('show');
+    });
+  }
+
+  links.forEach(elem => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      document.querySelector(`[id=${elem.dataset.id}]`).classList.add('show');
+    });
+  });
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+      hideModals();
+    });
+  });
+
+}
+
+showProjectDetails();
+
+// effets
+
+const observerIntersectionAnimation = () => {
+  const sections = document.querySelectorAll('section');
+  const skills = document.querySelectorAll('.skills .bar');
+
+  sections.forEach((section, index) => {
+    if (index === 0) return;
+    section.style.opacity = "0";
+    section.style.transition = "all 1.6s";
+  });
+
+  skills.forEach((elem, index) => {
+
+    elem.style.width = "0";
+    elem.style.transition = "all 1.6s";
+  });
+
+  let sectionObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        elem.style.opacity = 1;
+      }
+    });
+  });
+
+  sections.forEach(section => {
+    sectionObserver.observe(section);
+  });
+
+  let skillsObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        elem.style.width = elem.dataset.width + '%';
+      }
+    });
+  });
+
+  skills.forEach(skill => {
+    skillsObserver.observe(skill);
+  });
+}
+
+observerIntersectionAnimation();
